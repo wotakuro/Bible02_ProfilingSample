59903f95a13745afc185cd1f85401ee1247a8e63
Yusuke Kurokawa <ykurokawa@unity3d.com>
2023-11-02 20:15:18 +0900

    add plugin

---
+++ Assets/Examples/07_NativePluginCallback/PluginSource~/Source/Program.cpp
@@ -0,0 +1,139 @@
+// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+#include "stdlib.h"
+#include "UnityPluginHeaders/IUnityProfilerCallbacks.h"
+#include <iostream>
+#include <fstream>
+#include <mutex>
+
+// From PlatformDependSource
+long GetThreadId();
+
+static const int BufferSize = 1024 * 1024;
+static IUnityProfilerCallbacks* s_UnityProfilerCallbacks = NULL;
+
+static bool s_executeFlag = false;
+static char* buffer = NULL;
+static char* bufferForManagedCode = NULL;
+static int currentBufferIdx = 0;
+
+static std::mutex mtx;
+
+static void WriteFileShaderCompileGPUProgram(const UnityProfilerMarkerData* eventData) {
+
+    mtx.lock();
+
+    int size = 1024;
+
+    // [0] shader name
+    // [1] pass
+    // [2] keyword
+    int next = snprintf(buffer + currentBufferIdx, size, "%s,EditorCompile,%s,%s\n",
+        reinterpret_cast<const char*>(eventData[0].ptr),
+        reinterpret_cast<const char*>(eventData[1].ptr),
+        reinterpret_cast<const char*>(eventData[2].ptr));
+    mtx.unlock();
+}
+
+static void WriteFileShaderCreateGPUProgram(const UnityProfilerMarkerData* eventData) {
+    mtx.lock();
+    int size = 1024;
+    // [0] shader name
+    // [1] pass
+    // [2] stage
+    // [3] keyword
+    int next = snprintf(buffer + currentBufferIdx,size, "%s,%s,%s,%s\n",
+        reinterpret_cast<const char*>(eventData[0].ptr),
+        reinterpret_cast<const char*>(eventData[1].ptr),
+        reinterpret_cast<const char*>(eventData[2].ptr),
+        reinterpret_cast<const char*>(eventData[3].ptr) );
+    currentBufferIdx += next;
+    mtx.unlock();
+}
+
+static void UNITY_INTERFACE_API OnProfilerEvent(const UnityProfilerMarkerDesc* markerDesc, UnityProfilerMarkerEventType eventType, unsigned short eventDataCount, const UnityProfilerMarkerData* eventData, void* userData)
+{
+    if (!s_executeFlag) { return; }
+
+    switch (eventType)
+    {
+    case kUnityProfilerMarkerEventTypeBegin:
+    {
+
+        if (eventDataCount == 3 &&
+            strncmp(markerDesc->name, "Shader.CompileGPUProgram", 24) == 0)
+        {
+            WriteFileShaderCompileGPUProgram(eventData);
+        }else if (eventDataCount == 4 &&
+            strncmp(markerDesc->name, "Shader.CreateGPUProgram", 23) == 0)
+        {
+            WriteFileShaderCreateGPUProgram(eventData);
+        }
+
+        break;
+    }
+    case kUnityProfilerMarkerEventTypeEnd:
+        break;
+
+    }
+}
+
+extern "C" bool UNITY_INTERFACE_EXPORT  _NativeProfilerCallbackPluginUpdate()
+{
+    mtx.lock();
+    if (currentBufferIdx == 0) {
+        mtx.unlock();
+        return false;
+    }
+    memcpy(bufferForManagedCode, buffer, currentBufferIdx + 1);
+    currentBufferIdx = 0;
+    mtx.unlock();
+    return true;
+}
+extern "C" const char* _NativeProfilerCallbackPluginGetUpdateResult() {
+    return bufferForManagedCode;
+}
+
+
+extern "C" void UNITY_INTERFACE_EXPORT  _NativeProfilerCallbackPluginSetupBuffer()
+{
+    buffer = reinterpret_cast<char*>(malloc(BufferSize));
+    bufferForManagedCode = reinterpret_cast<char*>(malloc(BufferSize));
+}
+
+
+extern "C" void UNITY_INTERFACE_EXPORT _NativeProfilerCallbackPluginSetEnable(bool enable)
+{
+    s_executeFlag = enable;
+}
+
+
+
+static void UNITY_INTERFACE_API SetupCreateMarkerCallback(const UnityProfilerMarkerDesc* markerDesc, void* userData)
+{
+    if( strncmp(markerDesc->name, "Shader.CompileGPUProgram", 24) == 0 ||
+        strncmp(markerDesc->name, "Shader.CreateGPUProgram", 23) == 0 )
+        {
+            s_UnityProfilerCallbacks->RegisterMarkerEventCallback(markerDesc, OnProfilerEvent, NULL);
+        }
+}
+
+static bool s_IsLoadedPlugin = false;
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces * unityInterfaces)
+{
+    if (!s_IsLoadedPlugin) {
+        s_UnityProfilerCallbacks = unityInterfaces->Get<IUnityProfilerCallbacks>();
+        s_UnityProfilerCallbacks->RegisterCreateMarkerCallback(&SetupCreateMarkerCallback, NULL);
+        s_IsLoadedPlugin = true;
+    }
+}
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+{
+    if (s_IsLoadedPlugin) {
+        s_UnityProfilerCallbacks->UnregisterCreateMarkerCallback(&SetupCreateMarkerCallback, NULL);
+        s_UnityProfilerCallbacks->UnregisterMarkerEventCallback(NULL, &OnProfilerEvent, NULL);
+        s_IsLoadedPlugin = false;
+    }
+}
+
---
+++ Assets/Examples/07_NativePluginCallback/PluginSource~/WinSource/dllmain.cpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+// Windows ヘッダー ファイル
+#include <windows.h>
+
+#include <iostream>
+#include <fstream>
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+long GetThreadId() {
+
+    return GetCurrentThreadId();
+}
+
+
+
+
