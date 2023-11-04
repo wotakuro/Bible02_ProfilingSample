f5043ff234b6e392769f25d4f069ffd54d8acb90
Yusuke Kurokawa <wotakuro@gmail.com>
2023-11-05 01:28:52 +0900

    update sample 11

---
+++ Assets/Examples/11_FrameMetaData/Script/Editor/GetFrameMetadataSample.cs
@@ -0,0 +1,98 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEditor;
+using UnityEditor.Profiling;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GetFrameMetadataSample : EditorWindow
+{
+    struct TextureInfo
+    {
+        public int format;
+        public int w;
+        public int h;
+    }
+    [MenuItem("Example/11_FrameMetaData")]
+    public static void Create()
+    {
+        GetFrameMetadataSample.GetWindow<GetFrameMetadataSample>();
+    }
+
+    // Editor上のコードです
+    // GUID、EmitFrameMetaDataとGetFrameMetaDataで一致させる必要があります
+    Guid MetadataId = new Guid("7E1DEA84-51F1-477A-82B5-B5C57AC1EBF7");
+    // FrameMetaDataはTagIDでデータを区別できます。
+    // Textureの情報を埋め込まれたTag
+    static readonly int TextureInfoTag = 0;
+    // Textureの本体を埋め込まれたTag
+    static readonly int TextureBodyTag = 1;
+
+    private void OnEnable()
+    {
+        ProfilerWindow.GetWindow<ProfilerWindow>().SelectedFrameIndexChanged += (frame) =>
+        {
+            this.Repaint();
+        };
+    }
+    private Texture2D screenshotCache;
+    private int cacheFrameIdx = -1;
+
+    // Update is called once per frame
+    void OnGUI()
+    {        
+        // ProfilerWindowで現在選択中のフレームのIndex
+        int frameIdx = (int)ProfilerWindow.GetWindow<ProfilerWindow>().selectedFrameIndex;
+        if (cacheFrameIdx != frameIdx)
+        {
+            screenshotCache = GetScreenshotTexture(frameIdx);
+            cacheFrameIdx = frameIdx;
+        }
+        if (screenshotCache)
+        {
+            EditorGUI.DrawPreviewTexture(new Rect(10, 10, screenshotCache.width, screenshotCache.height), screenshotCache);
+
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Screenshotのデータが埋め込まれていません");
+            EditorGUILayout.LabelField("11_FrameMetaDataのシーンで実行してProfilerにScreenshotを埋め込んでください");
+        }
+
+    }
+    private Texture2D GetScreenshotTexture(int frameIdx)
+    {
+
+        // ProfilerWindowの現在選択中のフレームの情報を得ます
+        using (var frameData = ProfilerDriver.GetHierarchyFrameDataView(frameIdx, 0,
+            HierarchyFrameDataView.ViewModes.Default,
+            HierarchyFrameDataView.columnDontSort, false))
+        {
+            // フレームに埋め込まれた MetadataId のタグ０番をTextureInfo型として取得します
+            NativeArray<TextureInfo> textureInfos =
+                frameData.GetFrameMetaData<TextureInfo>(MetadataId, TextureInfoTag);
+
+            if (textureInfos == null || textureInfos.Length == 0)
+            {
+                return null;
+            }
+
+            // Textureの情報からTextureを作成します
+            TextureInfo textureInfo = textureInfos[0];
+            Texture2D texture2D = new Texture2D(textureInfo.w, textureInfo.h,
+                (TextureFormat)textureInfo.format, false);
+
+            // フレームに埋め込まれた MetadataId のタグ1番をByte配列として取得します
+            NativeArray<byte> textureData =
+                frameData.GetFrameMetaData<byte>(MetadataId, TextureBodyTag);
+            // Textureの本体を元にTextureの情報をロードします
+            texture2D.LoadRawTextureData(textureData);
+            texture2D.Apply();
+            return texture2D;
+        }
+
+    }
+}
--- Assets/Examples/11_FrameMetaData/Script/Runtime/EmitFrameMetadataSample.cs
+++ Assets/Examples/11_FrameMetaData/Script/Runtime/EmitFrameMetadataSample.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Unity.Collections;
+using UnityEngine.Profiling;
 
 public class EmitFrameMetadataSample : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{// ランタイム上で実行されるコードです
+ // FrameDataView.GetFrameMetadataと共通して使います
+    struct TextureInfo
     {
-        
+        public int format;
+        public int w;
+        public int h;
+    }
+    // GUID、EmitFrameMetaDataとGetFrameMetaDataで一致させる必要があります
+    static readonly Guid MetadataId = new Guid("7E1DEA84-51F1-477A-82B5-B5C57AC1EBF7");
+    // FrameMetaDataはTagIDでデータを区別できます。
+    // Textureの情報を埋め込むTag
+    static readonly int TextureInfoTag = 0;
+    // Textureの本体を埋め込むTag
+    static readonly int TextureBodyTag = 1;
+
+    public Texture2D tex;
+
+
+    public void LateUpdate()
+    {
+        StartCoroutine(RecordFrame());
+    }
+    // Update is called once per frame
+
+    IEnumerator RecordFrame()
+    {
+        yield return new WaitForEndOfFrame();
+
+        var texture2D = ScreenCapture.CaptureScreenshotAsTexture(1);
+        tex = texture2D;
+        Debug.Log(tex.isReadable);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var texture2D = tex;
+        // Texture2Dの情報をTag 0番に埋め込みます
+        TextureInfo textureInfo = new TextureInfo()
+        {
+            format = (int)texture2D.format,
+            w = texture2D.width,
+            h = texture2D.height
+        };
+        Profiler.EmitFrameMetaData(MetadataId, TextureInfoTag, new[] { textureInfo });
+
+        // Texture本体のデータを取得し、Tag１番にデータを埋め込みます
+        NativeArray<byte> textureData = texture2D.GetRawTextureData<byte>();
+        Profiler.EmitFrameMetaData(MetadataId, TextureBodyTag, textureData);
+
+
     }
 }
